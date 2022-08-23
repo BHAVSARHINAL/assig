6310ae749ceda45a9a9fd606ac38ed7e35f06a1d
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-23 20:04:12 +0530

    Add files via upload

---
+++ nesdtedif.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int a=33,b=45,c=700;
+	
+	 if(a>b)
+	{
+		if(a>c)
+		{
+			printf("%d",a);
+		}
+	    
+	    else
+	    {
+	    	
+	    printf("%d",c);
+	    
+		}
+	}
+	
+	 else if(b>c)
+	{
+		printf("%d",b);
+		
+	}
+	else
+	
+	{
+		printf("%d",c);
+	}
+	 
+	 return 0;
+	
+	
+	
+	
+	
+	
+	
+}
